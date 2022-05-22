d50e42528d8eae020cd8666e1e71736cb977d1e8
bryanhennes <bryanhennes@gmail.com>
2022-05-22 12:35:52 -0400

    Linked app to firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import LoginForm from "./LoginForm";
+import { db, auth } from "./firebase-config";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut }  from "firebase/auth";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+
+
 
 function App() {
+  const register = async () => {
+    try{
+      const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+      console.log(user);
+    }catch (error){
+      console.log(error.message);
+    }
+  };
+
+  const login = async () => {
+    try{
+      const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+      console.log(user);
+    }catch (error){
+      console.log(error.message);
+    }
+  }
+
+  const logout = async () => {
+    await signOut(auth);
+  }
+  
+  //newName is input from name input field...useState is default state of name input which should be empty string
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginEmail, setloginEmail] = useState("");
+  const [loginPassword, setloginPassword] = useState("");
+  
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
+  const usersCollectionRef = collection(db, "users");
+
+  const [user, setUser] = useState({});
+
+  useEffect(()=> {
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+});
+
+
+  //display current users from firebase on page
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    }
+
+    getUsers();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="registerUser">
+        <h3>Register User</h3>
+        <input placeholder="Email..." onChange={(e) => {
+          setRegisterEmail(e.target.value);
+        }}/>
+        <input placeholder="Password..." onChange={(e) => {
+          setRegisterPassword(e.target.value);
+        }}/>
+        <button onClick={register}>Create User</button> 
+      </div>
+
+      <div className="login">
+        <h3>Login</h3>
+        <input placeholder="Email..." onChange={(e) => {
+          setloginEmail(e.target.value);
+        }}/>
+        <input placeholder="Password..." onChange={(e) => {
+          setloginPassword(e.target.value);
+        }}/>
+        <button onClick={login}>Login</button>
+
+        <h4>User Logged In:</h4>
+        <p>{user?.email}</p>
+
+        <button onClick={logout}>Sign Out</button>
+      </div>
+    
+
+    </>
+  )
 }
 
 export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC3fyo0T3MIHWL_zeNS6OKx_Tton1vi4jY",
+    authDomain: "quizzer-389c0.firebaseapp.com",
+    projectId: "quizzer-389c0",
+    storageBucket: "quizzer-389c0.appspot.com",
+    messagingSenderId: "825155381138",
+    appId: "1:825155381138:web:22b1fc2e2dd53305b1034b",
+    measurementId: "G-PESGY29SQH"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  export const db = getFirestore(app);
+  export const auth = getAuth(app);
