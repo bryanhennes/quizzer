1cb58afd9d2227a138ea75c37c1710bf651c809d
bryanhennes <bryanhennes@gmail.com>
2022-05-26 08:34:44 -0400

    read data from firebase functioning

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,30 +1,70 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react';
+import { connectAuthEmulator, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile }  from "firebase/auth";
+import { db, auth, realDb } from "./firebase-config";
+import { collection, getDocs, addDoc, doc, updateDoc} from "firebase/firestore";
+import {getDatabase, ref, set, child, update, remove, onValue} from "firebase/database";
 
 export default function LoginForm({Login, error}) {
-    const [details, setDetails] = useState({name: "", email: "", password: ""});
+    const [users, setUsers] = useState([]);
+    const [newName, setNewName] = useState("");
+    const [newUn, setNewUn] = useState("");
+    const [newCountry, setNewCountry] = useState("");
+    const [userId, setUid] = useState({});
+
+    let today = new Date();
+    let date=parseInt(today.getMonth()+1) + "/" + today.getDate() + "/"+today.getFullYear();
+
+    useEffect(()=> {
+        onAuthStateChanged(auth, (currentUser) => {
+          setUser(currentUser);
+          
+        });
+      });
+    const [user, setUser] = useState({});
+  
+
+    const createUser = async () => {
+      console.log(user.uid);
+      await set(ref(realDb, 'users/' + user.uid),{
+				firstname: newName,
+				username: newUn,
+				country: newCountry,
+        email: user?.email,
+        joindate: date
+      })
+    };
+
+
+    //save users information in database
     const submitHandler = e => {
         e.preventDefault();
-        Login(details);
+       
+        alert("Information saved");
     }
+
+
   return (
-    <form onSubmit={submitHandler}>
+    
         <div className="form-inner">
-            <h2>Login</h2>
+            <h1>Welcome, {user?.email} </h1>
+            <h2>Add Your Info</h2>
             {/* Error*/}
             <div className="form-group">
-                <label htmlFor="name">Name:</label>
-                <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
+                <input type="text" placeholder="Name" name="name" id="name" onChange={e => setNewName(e.target.value)}/>
             </div>
+            <br></br>
             <div className="form-group">
-                <label htmlFor='email'>Email:</label>
-                <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
+                <input type="text" placeholder='Username' name="username" id="username" onChange={e => setNewUn(e.target.value)}/>
             </div>
+            <br></br>
             <div className="form-group">
-                <label htmlFor='password'>Password:</label>
-                <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+                <input type="text" placeholder='Country' name="country" id="country" onChange={e => setNewCountry(e.target.value)}/>
             </div>
-            <input type="submit" value="LOGIN"/>
+            <br></br>
+            <button className="submitButton" onClick={createUser}></button>
+            
         </div>
-    </form>
+        
+ 
   )
 }
